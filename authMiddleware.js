8d4dc149e0ef83ad588b6ef3757273505e91960f
dihiaflz <d_falouz@estin.dz>
2023-12-30 22:17:50 +0100

    minor changes

--- authMiddleware.js
+++ authMiddleware.js
@@ -3,16 +3,30 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization
+  const authBody = req.body.token ? req.body.token : null
+  console.log(authHeader)
   try {
-    if (!authHeader) {
-      return res.status(401).json({ error: 'Unauthorized - Missing Token' });
+    if(authBody == null){
+        if (!authHeader) {
+          return res.status(401).json({ error: 'Unauthorized - Missing Token' });
+        }
+        const data = jwt.verify(authHeader, process.env.SECRET);
+        if (!data.user) {
+          return res.status(401).json({ error: 'Unauthorized - Invalid Token' });
+        }
+        req.user = data.user
+        next();
+    }else{
+        if (!authBody) {
+          return res.status(401).json({ error: 'Unauthorized - Missing Token' });
+        }
+        const data = jwt.verify(authBody, process.env.SECRET);
+        if (!data.user) {
+          return res.status(401).json({ error: 'Unauthorized - Invalid Token' });
+        }
+        req.user = data.user
+        next();
     }
-    const data = jwt.verify(authHeader, process.env.SECRET);
-    if (!data.user) {
-      return res.status(401).json({ error: 'Unauthorized - Invalid Token' });
-    }
-    req.user = data.user
-    next();
   } catch (error) {
     if (error.name === 'TokenExpiredError') {
       return res.status(401).json({ error: 'Unauthorized - Token Expired' });
